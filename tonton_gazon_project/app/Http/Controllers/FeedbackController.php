b98fbd3c4df34b056725962c58360ec96a0494df
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2020-01-23 20:26:26 +0100

    Feedback.php and FeedbackController.php added, methods fetchFeedback

---
+++ tonton_gazon_project/app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Feedback;
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+
+    /**
+     * Retrieve all the feedbacks of the application
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function fetchFeedback() {
+        $feedback = Feedback::get();
+
+        return response(['feedback' => $feedback],200);
+    }
+}
