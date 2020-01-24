03c85483869db9c55bea2aa2d652b993e5eb525d
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2020-01-24 12:09:08 +0100

    component adverts

---
+++ tonton_gazon_project/resources/js/components/adverts/Advert.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default function Advert() {
+
+    const [adverts, setAdverts] = useState([]);
+
+    useEffect(() => {
+        axios.get('api/adverts'
+        ).then(res => {
+            setAdverts(res.data.advert);
+        });
+    }, []);
+
+    let jsx = adverts.map((adv) => {
+        return(
+            <div>
+                <h1>{adv.title}</h1>
+                <span>{adv.description}</span>
+            </div>
+        )
+    });
+    return (
+        <div>
+            {jsx}
+        </div>
+    )
+}
