d4c4fd6d5a8e883b19b2f1b822152884f5a26cb5
DondaineMathieu <dondaine.mathieu@gmail.com>
2020-02-09 19:45:03 +0100

    adding variable color for error and succes, html and css template for form

--- tonton_gazon_project/resources/js/components/FormTemplate.js
+++ tonton_gazon_project/resources/js/components/FormTemplate.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="container container_form">
             <Nav link="Logout"/>
 
-            <div className="bloc bloc_form">
+            <form className="bloc bloc_form">
                 <div className="bloc_title">
                     <img src="./img/waving-hand-sign.png"></img>
                     <h3>Formulaire type</h3>
@@ -42,10 +42,66 @@ export default function Home() {
 
                 <div className="form_group">
                     <label className="form_label">Label type avec text area</label>
-                    <input className="form_input error" type="textarea" rows="5" placeholder="ceci est un input"></input>
+                    <textarea className="form_input"cols="30" rows="5" placeholder="ceci est un input"></textarea>
                 </div>
 
-            </div>
+                <div className="form_group">
+                    <p className="form_label">Label type avec checkbox</p>
+                    
+                    <div class="checkbox_group">
+                        <label class="control control-checkbox">
+                            First checkbox
+                                <input type="checkbox" checked="checked" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-checkbox">
+                            Second checkbox
+                                <input type="checkbox" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-checkbox">
+                            Third checkbox
+                                <input type="checkbox" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-checkbox">
+                            Disabled & checked
+                                <input type="checkbox" disabled="disabled" checked="checked" />
+                            <div class="control_indicator"></div>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="form_group">
+                    <p className="form_label">Label type avec checkbox</p>
+                    
+                    <div class="radio_group">
+                        <label class="control control-radio">
+                            First radio
+                                <input type="radio" name="radio" checked="checked" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-radio">
+                            Second radio
+                                <input type="radio" name="radio" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-radio">
+                            Disabled
+                                <input type="radio" name="radio2" disabled="disabled" />
+                            <div class="control_indicator"></div>
+                        </label>
+                        <label class="control control-radio">
+                            Disabled & checked
+                                <input type="radio" name="radio2" disabled="disabled" checked="checked" />
+                            <div class="control_indicator"></div>
+                        </label>
+                    </div>
+                </div>
+
+                <a href="" className="btn btn_primary">Envoyer le formulaire</a>
+                <a href="" className="btn btn_secondary">Annuler le fomulaire</a>
+            </form>
 
             <Footer/>
         </div>
