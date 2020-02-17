6cf3e2c700124a1d29ca81335227b876e8a9e5ba
DondaineMathieu <dondaine.mathieu@gmail.com>
2020-02-17 10:29:39 +0100

    navbar mobile

--- tonton_gazon_project/resources/js/script/script.js
+++ tonton_gazon_project/resources/js/script/script.js
@@ -1,1 +1,14 @@
+import $ from 'jquery'
 import './sliders'
+
+$(function() {
+    $('.navbar_hamburger').click( function() {
+        if( $('.navbar_mobile').hasClass('inactive') ) {
+            $('.navbar_mobile').removeClass('inactive');
+            $('.navbar_mobile').addClass('active');
+        } else {
+            $('.navbar_mobile').addClass('inactive');
+            $('.navbar_mobile').removeClass('active');
+        }
+    });
+});
