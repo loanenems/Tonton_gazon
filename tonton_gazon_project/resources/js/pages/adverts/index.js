29e78316c5ba5979695c71e8485ec662f7ce8a96
DondaineMathieu <dondaine.mathieu@gmail.com>
2020-03-24 13:37:55 +0100

    advert listing style

--- tonton_gazon_project/resources/js/pages/adverts/index.js
+++ tonton_gazon_project/resources/js/pages/adverts/index.js
@@ -12,19 +12,45 @@ export default function Index() {
             (res.data.data);
         });
     }, []);
+    
+    console.log(data);
 
     let jsx = data.map((data) => {
         return (
-            <div>
-                <h1>{data.Advert.title}</h1>
-                <span>{data.Advert.description}</span>
-            </div>
+            // Image
+            // Nom utilisateur
+            // Prix
+            // Evaluation
+            // Superficie
+
+            <a href="" className="advert_solo_container">
+                {/* // Image */}
+                <img src="./img/jardin.jpg"></img>
+                {/* <p>{data.Garden.image}</p> */}
+                <div className="advert_infos">
+                    <div className="infos_first">
+                        {/* // Nom utilisateur */}
+                        <p className="info_name">{data.User.name}</p>
+                        {/* // Prix */}
+                        <p className="info_payout">{data.Advert.payout}€</p>
+                    </div>
+                    <div className="infos_description">
+                        <p className="info_payout">{data.Advert.description}</p>
+                    </div>
+                    <div className="infos_second">
+                        {/* // Evaluation */}
+                        <p className="info_note"> <img src="./img/etoile_verte.svg"/>{data.User.xp} <mark>(23)</mark></p>
+                        {/* // Superficie */}
+                        <p className="info_size"><mark>Superficie : </mark>{data.Garden.size}/m*</p>
+                    </div>
+                </div>
+            </a>
         )
     });
 
 
     return (
-        <div>
+        <div className="advert_list_container_row">
             {jsx}
         </div>
     )
