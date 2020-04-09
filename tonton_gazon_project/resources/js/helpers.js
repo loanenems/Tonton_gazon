2ff5ea1ba8a966d62eaa2da8b1b8e56aa58492db
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2020-04-09 17:17:14 +0200

    gesture of the javascript part to remove or add date fields

--- tonton_gazon_project/resources/js/helpers.js
+++ tonton_gazon_project/resources/js/helpers.js
@@ -1,6 +1,6 @@
 export function urlFromFilter(requestedPage = 1, pos = null) {
     let position = "";
-    if(pos !== null) {
+    if (pos !== null) {
         position = JSON.stringify({lat: pos.coords.latitude, lon: pos.coords.longitude});
     }
     //This function return the checked option.
@@ -41,4 +41,38 @@ export function urlFromFilter(requestedPage = 1, pos = null) {
     }
 
     return url;
+}
+
+export function addDynamicalForm() {
+    let nbDates = $('#date_group').find('input[type="date"]').length;
+    if (nbDates < 6) {
+        $('#date_group').append(
+            '<input class="form_input" type="date" name="date_' + nbDates + '" id="date_' + nbDates + '"/>' +
+            '<button class="remove_item">X</button>'
+        );
+    }
+    $('.remove_item').unbind().click(function (e) {
+        e.preventDefault();
+        handleClick($(this));
+    });
+}
+
+function handleClick(elem) {
+    let nbDates = $('#date_group').find('input[type="date"]').length;
+    let currId = parseInt($(elem).prev().attr('id').split('_')[1]);
+    //If last
+    if (currId === nbDates - 1) {
+        $(elem).prev().remove();
+        $(elem).remove();
+    }
+    //Somewhere before the last
+    else {
+        console.log(currId);
+        for (let i = currId + 1; i <= (nbDates - 1); i++) {
+            $('#date_' + i).attr('name', 'date_' + (i - 1));
+            $('#date_' + i).attr('id', 'date_' + (i - 1));
+            $(elem).prev().remove();
+            $(elem).remove();
+        }
+    }
 }
