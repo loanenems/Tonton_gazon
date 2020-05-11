94a00e9dd735dbc2588eb6d0799d12828fc8f148
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2020-05-11 10:04:48 +0200

    remove useless imports in header.js

--- tonton_gazon_project/resources/js/components/header.js
+++ tonton_gazon_project/resources/js/components/header.js
@@ -1,10 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import {Link, useHistory} from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import axios from 'axios';
-import {urlFromFilter} from '../helpers'
 
 export default function Nav() {
-
     //We store the current loggin state into a var
     const [isLogged, setIsLogged] = useState('');
 
