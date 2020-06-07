1dd7cd1f4bc1848b193d10e987e9c55ef97f1571
Nemmiche Loane <loane_nemmiche@ens.univ-artois.fr>
2020-06-07 22:47:56 +0200

    fix advert

--- tonton_gazon_project/app/Http/Controllers/GardenController.php
+++ tonton_gazon_project/app/Http/Controllers/GardenController.php
@@ -80,7 +80,7 @@ class GardenController extends Controller
         }
 
         //We store the image in a folder named with the user id and file with original name
-        $validateData['image']->storeAs('public/' . auth()->id() . '/Advert', $validateData['image']->getClientOriginalName());
+        $validateData['image']->storeAs('public/' . auth()->id() . '/Advert_Index', $validateData['image']->getClientOriginalName());
 
         //Once there are no errors, we insert a new row in the Advert_Index table
         Garden::insert(
@@ -92,7 +92,7 @@ class GardenController extends Controller
                 'unmovableObstacle' => $validateData['unmovableObstacle'],
                 'pets' => $validateData['pets'],
                 'equipment' => $validateData['equipment'],
-                'image' => json_encode(['image_0' => asset('storage/' . auth()->id() . '/Advert/' . $validateData['image']->getClientOriginalName())]),
+                'image' => json_encode(['image_0' => asset('storage/' . auth()->id() . '/Advert_Index/' . $validateData['image']->getClientOriginalName())]),
                 'address' => $validateData['address'],
             ]
         );
